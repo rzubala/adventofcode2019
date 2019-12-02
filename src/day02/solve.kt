44a6b007747619e8ca732a98000c12dcea864e27
rzubala@gmail.com <rzubala@gmail.com>
2019-12-02 08:21:17 +0100

    day2 part2

--- src/day02/solve.kt
+++ src/day02/solve.kt
@@ -2,11 +2,30 @@ package day02
 
 import utils.readInput
 
+const val part2value = 19690720
+
 fun main() {
     val opcodes = readInput("src/day02/input.data")[0].split(",").map { it.toInt() }.toMutableList()
-    opcodes[1] = 12
-    opcodes[2] = 2
-    println(opcodes)
+    //part1
+    val list = mutableListOf(*opcodes.toTypedArray())
+    println("part1: ${calculate(list, 12, 2)}")
+
+    //part2
+    for(noun in 0..99) {
+        for(verb in 0..99) {
+            val list = mutableListOf(*opcodes.toTypedArray())
+            val res = calculate(list, noun, verb)
+            if (res == part2value) {
+                println("part2: ${noun * 100 + verb}")
+                return
+            }
+        }
+    }
+}
+
+private fun calculate(opcodes: MutableList<Int>, noun: Int, verb: Int): Int {
+    opcodes[1] = noun
+    opcodes[2] = verb
     for (i in 0..opcodes.size step 4) {
         val op = opcodes[i]
         if (op == 99) {
@@ -15,14 +34,14 @@ fun main() {
         val data1 = opcodes[i + 1]
         val data2 = opcodes[i + 2]
         val dst = opcodes[i + 3]
-        opcodes[dst] = when(op){
+        opcodes[dst] = when (op) {
             1 -> opcodes[data1] + opcodes[data2]
             2 -> opcodes[data1] * opcodes[data2]
             else -> {
                 println("error!")
-                opcodes[dst]
+                return -1
             }
         }
     }
-    println(opcodes[0])
+    return opcodes[0]
 }
