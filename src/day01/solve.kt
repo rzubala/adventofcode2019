c9b66fb1fb74bd99340bd3cae5fc7a30855dfc26
rzubala@gmail.com <rzubala@gmail.com>
2019-12-02 09:17:22 +0100

    refactor

--- src/day01/solve.kt
+++ src/day01/solve.kt
@@ -4,9 +4,9 @@ import utils.readInput
 
 fun main() {
     val lines = readInput("src/day01/input.data").map {Integer.valueOf(it)}
-    val res1 = lines.map { operation(it) }.sum()
+    val res1 = lines.sumBy{ operation(it) }
     println("sum: $res1")
-    val res2 = lines.map { operation2(it) }.sum()
+    val res2 = lines.sumBy { operation2(it) }
     println("sum: $res2")
 }
 
