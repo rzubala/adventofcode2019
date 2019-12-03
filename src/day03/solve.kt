f6bba829f1e9d407cb99d7e4d89a2a2d7ca44087
rzubala@gmail.com <rzubala@gmail.com>
2019-12-03 09:18:15 +0100

    day03 part 2

--- src/day03/solve.kt
+++ src/day03/solve.kt
@@ -5,10 +5,10 @@ import kotlin.math.abs
 
 data class Point(val x:Int, val y:Int)
 val moveMap: Map<Char, (Point, Int) -> List<Point>> = mapOf(
-        'U' to { point: Point, offset: Int -> range(point, offset) { point: Point -> Point(point.x, point.y + 1) } },
-        'D' to { point: Point, offset: Int -> range(point, offset) { point: Point -> Point(point.x, point.y - 1) } },
-        'L' to { point: Point, offset: Int -> range(point, offset) { point: Point -> Point(point.x - 1, point.y) } },
-        'R' to { point: Point, offset: Int -> range(point, offset) { point: Point -> Point(point.x + 1, point.y) } }
+        'U' to { point: Point, offset: Int -> range(point, offset) { p: Point -> Point(p.x, p.y + 1) } },
+        'D' to { point: Point, offset: Int -> range(point, offset) { p: Point -> Point(p.x, p.y - 1) } },
+        'L' to { point: Point, offset: Int -> range(point, offset) { p: Point -> Point(p.x - 1, p.y) } },
+        'R' to { point: Point, offset: Int -> range(point, offset) { p: Point -> Point(p.x + 1, p.y) } }
     )
 
 fun range(point: Point, offset: Int, op: (Point)->Point): List<Point> {
@@ -27,13 +27,29 @@ fun main() {
     val path2 = createPath(data[1].split(","))
 
     //part1
-    findMinDistance(path1, path2)
+    val crossings: List<Point> = findMinDistance(path1, path2)
+
+    //part2
+    findMinSteps(crossings, path1, path2)
 }
 
-fun findMinDistance(path1: List<Point>, path2: List<Point>) {
+fun findMinSteps(crossings: List<Point>, path1: List<Point>, path2: List<Point>) {
     var min = Integer.MAX_VALUE
+    crossings.forEach {
+        val sum: Int = path1.indexOf(it) + 1 + path2.indexOf(it) + 1
+        if (it != Point(0,0) && sum < min) {
+            min = sum
+        }
+    }
+    println("steps: $min")
+}
+
+fun findMinDistance(path1: List<Point>, path2: List<Point>): List<Point> {
+    var min = Integer.MAX_VALUE
+    val results: MutableList<Point> = mutableListOf()
     path1.forEach {
-        if (path2.contains(it)) {
+        if (it != Point(0,0) && path2.contains(it)) {
+            results.add(it)
             val dist = dist(it)
             if (dist < min) {
                 min = dist
@@ -41,6 +57,7 @@ fun findMinDistance(path1: List<Point>, path2: List<Point>) {
         }
     }
     println("distance: $min")
+    return results
 }
 
 inline fun dist(point: Point): Int = abs(point.x) + abs(point.y)
