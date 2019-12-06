8c5b043defedc096e62496ad0d8b998ffc83e116
rzubala@gmail.com <rzubala@gmail.com>
2019-12-06 08:21:58 +0100

    refactor

--- src/day06/solve.kt
+++ src/day06/solve.kt
@@ -8,51 +8,57 @@ fun main() {
     val orbits: MutableList<Orbit> = mutableListOf(Orbit("COM"))
     readInput("src/day06/input.data").forEach { row ->
         val data = row.split(")")
-        var parent = orbits.find { it.name == data[0] }
-        if (parent == null) {
-            parent = Orbit(data[0])
-            orbits.add(parent)
+        val parentPlanet = orbits.addOrbit(data[0])
+        val childPlanet = orbits.addOrbit(data[1])
+        childPlanet.apply {
+            parent = parentPlanet
         }
-        var child = orbits.find { it.name == data[1] }
-        if (child == null) {
-            child = Orbit(data[1])
-            orbits.add(child)
-        }
-        child.parent = parent
     }
     println("orbits: ${orbits.sumBy { it.countParents() }}")
-
     calculateTransfers(orbits, "YOU", "SAN")
 }
 
 fun calculateTransfers(orbits: MutableList<Orbit>, name1: String, name2: String) {
-    val p1 = orbits.find { it.name == name1 }
-    val p2 = orbits.find { it.name == name2 }
-    val parents1 = p1?.getParents()
-    val parents2 = p2?.getParents()
-    val common = parents1?.intersect(parents2!!)
-    val commonOrbit = common?.iterator()?.next()
-    commonOrbit?.let {
-        println("transfers: ${parents2?.indexOf(commonOrbit)?.let {result ->
-            parents1.indexOf(commonOrbit).plus(result)
+    val parents1 = orbits.find { it.name == name1 }?.getParents()
+    val parents2 = orbits.find { it.name == name2 }?.getParents()
+    parents1?.let {
+        parents2?.let {
+            val common = parents1.intersect(parents2)
+            val commonOrbit = common.iterator().next()
+            val transfers = parents2.indexOf(commonOrbit).let { result ->
+                parents1.indexOf(commonOrbit).plus(result)
+            }
+            println("transfers: $transfers")
         }
-        }")
     }
 }
 
 fun Orbit.getParents(): List<String> {
     val parents: MutableList<String> = mutableListOf()
-    var parent = parent
-    while (parent != null) {
-        parents.add(parent.name)
-        parent = parent.parent
-    }
+    fillParents(parents)
     return parents
 }
 
-fun Orbit.countParents(): Int {
-    if (parent == null) {
-        return 0
+fun Orbit.fillParents(list: MutableList<String>) {
+    parent?.let {
+        list.add(parent!!.name)
+        parent!!.fillParents(list)
     }
-    return 1 + parent!!.countParents()
+}
+
+fun Orbit.countParents(): Int {
+    parent?.let {
+        return parent!!.countParents().plus(1)
+    }
+    return 0
+}
+
+fun MutableList<Orbit>.addOrbit(name: String): Orbit {
+    var orbit = find { it.name == name }
+    orbit?.let{
+        return it
+    }
+    orbit = Orbit(name)
+    add(orbit)
+    return orbit
 }
