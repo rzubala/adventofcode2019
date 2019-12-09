9b2c60c6da4901bbc7a6dc5303bf91b734fe30d9
rzubala@gmail.com <rzubala@gmail.com>
2019-12-09 08:28:24 +0100

    day09 part2

--- src/day09/solve.kt
+++ src/day09/solve.kt
@@ -6,6 +6,6 @@ import utils.readInput
 
 fun main() {
     val opcodes = readInput("src/day09/input.data")[0].split(",").map { it.toLong() }
-    intCode(opcodes.copy(), {1L}) {out -> println(out)}
-
+    intCode(opcodes.copy(), {1}) {out -> println(out)}
+    intCode(opcodes.copy(), {2}) {out -> println(out)}
 }
