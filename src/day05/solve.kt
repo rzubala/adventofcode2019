e42e3d6e0912389e8c565e55267da13917ea6b26
rzubala@gmail.com <rzubala@gmail.com>
2019-12-09 08:23:56 +0100

    day08 part1

--- src/day05/solve.kt
+++ src/day05/solve.kt
@@ -17,19 +17,20 @@ fun intCode(opcodes: MutableList<Long>, input: () -> Long, output: (value: Long)
         val op = "${instruction[3]}${instruction[4]}".toLong()
         val mode1 = instruction[2].toString().toLong()
         val mode2 = instruction[1].toString().toLong()
+        val mode3 = instruction[0].toString().toLong()
         when (op) {
             1L -> {
-                val dst = opcodes[(i + 3).toInt()]
+                val dst = opcodes.getIndex(mode3, i + 3, relativeBase)
                 opcodes.setData(dst, opcodes.getData(i + 1, mode1, relativeBase) + opcodes.getData(i + 2, mode2, relativeBase))
                 i += 4
             }
             2L -> {
-                val dst = opcodes[(i + 3).toInt()]
+                val dst = opcodes.getIndex(mode3, i + 3, relativeBase)
                 opcodes.setData(dst, opcodes.getData(i + 1, mode1, relativeBase) * opcodes.getData(i + 2, mode2, relativeBase))
                 i += 4
             }
             3L -> {
-                val dst = opcodes[(i + 1).toInt()]
+                val dst = opcodes.getIndex(mode1, i + 1, relativeBase)
                 opcodes.setData(dst, input())
                 i += 2
             }
@@ -53,8 +54,8 @@ fun intCode(opcodes: MutableList<Long>, input: () -> Long, output: (value: Long)
                 }
             }
             7L -> {
-                val dst = opcodes[(i + 3).toInt()]
-                opcodes.setData(dst,if (opcodes.getData(i + 1, mode1, relativeBase) < opcodes.getData(i + 2, mode2, relativeBase)) {
+                val dst = opcodes.getIndex(mode3, i + 3, relativeBase)
+                opcodes.setData(dst, if (opcodes.getData(i + 1, mode1, relativeBase) < opcodes.getData(i + 2, mode2, relativeBase)) {
                     1
                 } else {
                     0
@@ -62,7 +63,7 @@ fun intCode(opcodes: MutableList<Long>, input: () -> Long, output: (value: Long)
                 i += 4
             }
             8L -> {
-                val dst = opcodes[(i + 3).toInt()]
+                val dst = opcodes.getIndex(mode3, i + 3, relativeBase)
                 opcodes.setData(dst,if (opcodes.getData(i + 1, mode1, relativeBase) == opcodes.getData(i + 2, mode2, relativeBase)) {
                     1
                 } else {
@@ -82,17 +83,21 @@ fun intCode(opcodes: MutableList<Long>, input: () -> Long, output: (value: Long)
     throw IllegalStateException("Illegal state")
 }
 
-fun MutableList<Long>.getData(pointer: Long, mode: Long, base: Long): Long {
-    val index = when(mode) {
-        0L -> this[pointer.toInt()]
-        1L -> pointer
-        2L -> this[pointer.toInt()] + base
-        else -> throw IllegalStateException("Mode $mode not supported")
-    }
+fun MutableList<Long>.getData(index: Long, mode: Long, base: Long): Long {
+    val index = getIndex(mode, index, base)
     if (index > this.size - 1) {
         this.grow(index)
     }
     return this[index.toInt()]
+}
+
+fun MutableList<Long>.getIndex(mode: Long, index: Long, base: Long): Long {
+    return when (mode) {
+        0L -> this[index.toInt()]
+        1L -> index
+        2L -> this[index.toInt()] + base
+        else -> throw IllegalStateException("Mode $mode not supported")
+    }
 }
 
 fun MutableList<Long>.setData(index: Long, value: Long) {
