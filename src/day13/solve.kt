3d88bda1d39d864b352f320d081c3079866eff73
rzubala@gmail.com <rzubala@gmail.com>
2019-12-13 07:06:25 +0100

    day13 part2

--- src/day13/solve.kt
+++ src/day13/solve.kt
@@ -5,30 +5,71 @@ import utils.IntCode
 import utils.copy
 import utils.readInput
 
-data class Tile(val id: Int)
-
-const val WALL = 1
 const val BLOCK = 2
 const val H_PADDLE = 3
 const val BALL = 4
 
+const val LEFT = -1L
+const val RIGHT = 1L
+
 fun main() {
     val opcodes = readInput("src/day13/input.data")[0].split(",").map { it.toLong() }
-    val board: MutableMap<Point, Tile> = mutableMapOf()
+    play(opcodes.copy())
+}
+
+private fun play(opcodes: MutableList<Long>) {
+    var paddle: Point? = null
+    var ball: Point? = null
+    var blocks = 0
+    opcodes[0] = 2
+
     class Game(private val code: List<Long>) {
         var outCnt = 0
-        val tmp = MutableList(3) { 0 }
+        val data = MutableList(3) { 0 }
         fun start(): Long {
-            return IntCode(code.copy()).run({ 0 }) { out ->
-                tmp[outCnt] = out.toInt()
+            return IntCode(code.copy()).run({ getInput() }) { out ->
+                data[outCnt] = out.toInt()
                 if (outCnt == 2) {
-                    board[Point(tmp[0], tmp[1])] = Tile(tmp[2])
+                    when {
+                        data[2] == BALL -> {
+                            ball = Point(data[0], data[1])
+                        }
+                        data[2] == H_PADDLE -> {
+                            paddle = Point(data[0], data[1])
+                        }
+                        data[2] == BLOCK -> {
+                            blocks++
+                        }
+                    }
                 }
                 outCnt = (outCnt + 1) % 3
             }
         }
+        private fun getInput(): Long {
+            ball?.let { itb->
+                paddle?.let {itp ->
+                    return when {
+                        itp.x < itb.x -> {
+                            RIGHT
+                        }
+                        itp.x > itb.x -> {
+                            LEFT
+                        }
+                        else -> {
+                            0
+                        }
+                    }
+                }
+            }
+            return 0
+        }
+        fun score() {
+            println("Blocks: $blocks")
+            println("Score ${data[2]}")
+        }
     }
-    Game(opcodes.copy()).start()
-    println(board.values.filter { it.id == BLOCK }.size)
+    Game(opcodes.copy()).apply {
+        start()
+        score()
+    }
 }
-
