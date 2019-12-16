58719436916503f840d9c0c9568e78ad361d10c0
rzubala@gmail.com <rzubala@gmail.com>
2019-12-16 11:20:30 +0100

    refactor

--- src/day16/solve.kt
+++ src/day16/solve.kt
@@ -4,47 +4,57 @@ import utils.readInput
 
 const val PHASES = 100
 
-fun main() {
-    val basePattern = mutableListOf(0, 1, 0, -1)
-    val data = readInput("src/day16/input.data")[0].toCharArray().map { it.toString().toInt() }
-    var result = data
+val basePattern = mutableListOf(0, 1, 0, -1)
 
+fun main() {
+    val data = readInput("src/day16/input.data")[0].toCharArray().map { it.toString().toInt() }.toMutableList()
+
+    //Part1
+    var result = data
     (0 until PHASES).forEach { _ ->
-        result = calculatePhase(result, basePattern)
+        result = calculatePhase(result)
     }
-    println(result.subList(0, 8).joinToString("") { it.toString() })
+    println("Part1 ${result.subList(0, 8).joinToString("") { it.toString() }}")
+
+    //Part2
+    /*
+    val input = mutableListOf<Int>()
+    (0 until 10000).forEach { _ ->
+        input.addAll(data)
+    }
+    var result = input
+    (0 until PHASES).forEach { p ->
+        println("phase $p")
+        result = calculatePhase(result)
+    }
+    */
 }
 
-private fun calculatePhase(data: List<Int>, basePattern: MutableList<Int>): List<Int> {
-    val size = data.size
+private fun calculatePhase(data: List<Int>): MutableList<Int> {
     val result = mutableListOf<Int>()
-    (1..size).forEach { n ->
-        val pattern = createPatter(basePattern, n, size)
-        val value = calculate(data, pattern)
+    data.indices.forEach { n ->
+        val value = calculate2(data, n)
         result.add(value)
     }
     return result
 }
 
-fun calculate(data: List<Int>, pattern: List<Int>): Int {
+fun calculate2(data: List<Int>, outPosition: Int): Int {
     var sum = 0
-    data.forEachIndexed { i, v ->
-        sum += (v.times(pattern[i]))
+    data.forEachIndexed { inPosition, v ->
+        if (v != 0) {
+            val pattern = getPattern(outPosition, inPosition)
+            if (pattern != null) {
+                sum += (v.times(pattern))
+            }
+        }
     }
     return sum.toString().toCharArray().last().toString().toInt()
 }
 
-fun createPatter(pattern: List<Int>, pos: Int, size: Int): List<Int> {
-    val result = mutableListOf<Int>()
-    while (true) {
-        pattern.forEach { i ->
-            (0 until pos).forEach { _ ->
-                result.add(i)
-            }
-        }
-        if (result.size > size) {
-            break
-        }
-    }
-    return result.drop(1)
+fun getPattern(outP: Int, inP: Int): Int {
+    val repeat = outP.plus(1)
+    val patternSize = repeat.times(basePattern.size)
+    val index = (inP.plus(1) % patternSize).div(repeat)
+    return basePattern[index]
 }
