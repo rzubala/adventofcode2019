5226bd696d5b040c97cea8868cd29d174d7247fe
rzubala@gmail.com <rzubala@gmail.com>
2019-12-19 06:32:14 +0100

    day19 part 1

--- src/day19/solve.kt
+++ src/day19/solve.kt
@@ -1,24 +1,29 @@
 package day19
 
+import day10.Point
 import utils.IntCode
 import utils.copy
 import utils.readInput
 
+const val LAST_INDEX = 49
+
+
 fun main() {
     val code = readInput("src/day19/input.data")[0].split(",").map { it.toLong() }
 
-    class Processor {
-        fun start() {
-            IntCode(code.copy()).run({ getInput() }) { out ->
-                println(out)
+    var sum = 0L
+    (0..LAST_INDEX).forEach { y ->
+        (0..LAST_INDEX).forEach { x->
+            val position = Point(x,y)
+            var isX = true
+            IntCode(code.copy()).run({
+                val value = if (isX) position.x.toLong() else position.y.toLong()
+                isX = !isX
+                value
+            }) { out ->
+                sum += out
             }
         }
-        private fun getInput(): Long {
-            return 0L
-        }
     }
-
-    Processor().apply {
-        start()
-    }
+    println("sum $sum")
 }
