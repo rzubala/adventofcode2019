3b7a22772beca975260e5a8cce3c1221c25ff495
rzubala@gmail.com <rzubala@gmail.com>
2019-12-23 12:31:02 +0100

    refactor

--- src/day23/solve.kt
+++ src/day23/solve.kt
@@ -9,105 +9,104 @@ data class Packet(var x: Long, var y: Long = -1L, var hasY: Boolean = true, var 
 
 const val LAST = 49
 
+private val data: MutableMap<Int, MutableList<Packet>> = mutableMapOf()
+
 fun main() {
     val code = readInput("src/day23/input.data")[0].split(",").map { it.toLong() }
-
-    val data: MutableMap<Int, MutableList<Packet>> = mutableMapOf()
     (0..LAST).forEach { i ->
-        data[i] = mutableListOf<Packet>().apply { add(Packet(i.toLong(), hasY = false)) }
-    }
-
-    class Nat {
-        var packet = Packet(-1, -1)
-        var lastY = -1L
-        var start: Boolean = false
-            set(value) {
-                if (value && !start) {
-                    println("NAT start ${packet.y}")
-                    run()
-                    field = value
-                }
-            }
-        fun run() {
-            println("NAT run")
-            thread(start=true) {
-                while(true) {
-                    Thread.sleep(10)
-                    monitor()
-                }
-            }
-        }
-        fun monitor() {
-            if (start && packet.x > 0 && packet.y > 0 && isIdle()) {
-                data[0]?.add(packet.copy())
-                if (lastY == packet.y) {
-                    println("NAT repeat ${packet.y}")
-                }
-                lastY = packet.y
-                packet.x = -1
-                packet.y = -1
-            }
-        }
-
-        fun isIdle(): Boolean {
-            data.keys.forEach{ k ->
-                if (data[k]!!.isNotEmpty()) {
-                    return false
-                }
-            }
-            return true
-        }
+       data[i] = mutableListOf<Packet>().apply { add(Packet(i.toLong(), hasY = false)) }
     }
     val nat = Nat()
+    (0..LAST).forEach { i ->
+        thread(start = true) {
+            Computer(i, code.copy(), nat).start()
+        }
+    }
+}
 
-    class Computer(val address: Int) {
-        var to: Int = 0
-        var outCnt = 0
-        val packet = Packet(-1, -1)
-        fun start(): Long {
-            return IntCode(code.copy()).run({
-                getInput()
-            }
-            ) { out ->
-                when(outCnt) {
-                    0 -> to = out.toInt()
-                    1 -> packet.x = out
-                    2 -> {
-                        packet.y = out
-                        if (to > LAST) {
-                            nat.packet = packet.copy()
-                            nat.start = true
-                        } else {
-                            data[to]?.add(packet.copy())
-                        }
-                    }
-                }
-                outCnt = (outCnt + 1) % 3
+class Nat {
+    var packet = Packet(-1, -1)
+    private var lastY = -1L
+    var start: Boolean = false
+        set(value) {
+            if (value && !start) {
+                println("NAT start ${packet.y}")
+                run()
+                field = value
             }
         }
-        private fun getInput(): Long {
-            val input = data[address]!!
-            if (input.isEmpty()) {
-                return -1L
-            }
-            val packet = input[0]
-            return if (packet.readX) {
-                input.removeAt(0)
-                if (packet.hasY) {
-                    packet.y
-                } else {
-                    getInput()
-                }
-            } else {
-                packet.readX = true
-                packet.x
+    fun run() {
+        println("NAT run")
+        thread(start=true) {
+            while(true) {
+                Thread.sleep(200)
+                monitor()
             }
         }
     }
+    private fun monitor() {
+        if (start && packet.x > 0 && packet.y > 0 && isIdle()) {
+            data[0]?.add(packet.copy())
+            if (lastY == packet.y) {
+                println("NAT repeat ${packet.y}")
+            }
+            lastY = packet.y
+            packet.x = -1
+            packet.y = -1
+        }
+    }
 
-    (0..LAST).forEach { i ->
-        thread(start = true) {
-            Computer(i).start()
+    private fun isIdle(): Boolean {
+        data.keys.forEach{ k ->
+            if (data[k]!!.isNotEmpty()) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class Computer(private val address: Int, private val code: MutableList<Long>, private val nat: Nat) {
+    var to: Int = 0
+    var outCnt = 0
+    private val packet = Packet(-1, -1)
+    fun start(): Long {
+        return IntCode(code).run({
+            getInput()
+        }
+        ) { out ->
+            when(outCnt) {
+                0 -> to = out.toInt()
+                1 -> packet.x = out
+                2 -> {
+                    packet.y = out
+                    if (to > LAST) {
+                        nat.packet = packet.copy()
+                        nat.start = true
+                    } else {
+                        data[to]?.add(packet.copy())
+                    }
+                }
+            }
+            outCnt = (outCnt + 1) % 3
+        }
+    }
+    private fun getInput(): Long {
+        val input = data[address]!!
+        if (input.isEmpty()) {
+            return -1L
+        }
+        val packet = input[0]
+        return if (packet.readX) {
+            input.removeAt(0)
+            if (packet.hasY) {
+                packet.y
+            } else {
+                getInput()
+            }
+        } else {
+            packet.readX = true
+            packet.x
         }
     }
 }
